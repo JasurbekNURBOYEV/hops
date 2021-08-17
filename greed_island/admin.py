2c1a0bf712ae2cc882628b7fce58d7950c985287
Jasur Nurboyev <bluestacks6523@gmail.com>
2021-08-18 00:24:32 +0500

    Added: admin: question can be deleted by (only) super admin

--- greed_island/admin.py
+++ greed_island/admin.py
@@ -68,8 +68,5 @@ class QuestionAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
     def has_change_permission(self, request, obj=None):
         return False

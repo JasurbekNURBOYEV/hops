f5151dc7da1b203578384b3ef52e14b0adb1aae4
Husniddin Murodov <73847672+hoosnick@users.noreply.github.com>
2022-05-08 20:30:09 +0500

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -12,12 +12,14 @@ import inspect
 
 def lock_method_for_strangers(
         checker: Callable[[int, List[int]], bool],
+        stop_bloggers: Callable = None,
         default: Callable = None,
         white_list: List[int] = None
 ) -> Callable:
     """
     Used to lock a specific method in order to make the method inaccessible to specifici users
     :param checker: a callable object for doing the checking process
+	:param stop_bloggers: a callable object like checker parameter
     :param default: default function to call when lock is necessary
     :param white_list: a list of tg user ids
     :return: function object
@@ -33,12 +35,15 @@ def lock_method_for_strangers(
             # method seems to be lockable, let's try to lock it
             def lockable(message, *args, **kwargs):
                 # if chat is not in whitelist, we need to lock it
-                if checker(message.from_user.id, white_list):
-                    # method is not locked
-                    return method(message, *args, **kwargs)
+                is_member = True if checker(message.from_user.id, white_list) else False
+                is_blogger = True if stop_bloggers(message) else False
+				
+				if is_member and is_blogger:
+					# method is not locked
+					return method(message, *args, **kwargs)
                 # method is locked
                 # now we'll try doing a default process
-                return default(message, *args, **kwargs) if default else None
+                return default(message, is_blogger, *args, **kwargs) if default else None
             return lockable
         else:
             # method is not lockable, we return the original version back without any changes

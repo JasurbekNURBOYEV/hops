1547c8b27ca5d6830649c0821d15b4d2950dea30
Jasurbek NURBOYEV <jasurbeknurboyev@gmail.com>
2023-08-08 17:22:04 +0500

    Added filter to kick chinese userbots to prevent flood in group

--- core/mebers_gateway.py
+++ core/mebers_gateway.py
@@ -1,7 +1,15 @@
 """Used to filter new chat members."""
 import re
+from telebot.types import User
 
 
-def is_chinese_name(name: str) -> bool:
+def make_full_user_name(user: User) -> str:
+    name = user.first_name
+    if user.last_name:
+        name = f"{name} {user.last_name}"
+    return name
+
+
+def is_chinese_name(user: User) -> bool:
     """Check if there are at least 3 chinese letters in a row"""
-    return bool(re.search("[\u4e00-\u9fff]{3,}", name))
+    return bool(re.search("[\u4e00-\u9fff]{3,}", make_full_user_name(user)))

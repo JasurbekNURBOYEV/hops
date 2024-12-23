1b2747c7d5cc9d3c4c9bd99d0e8a4721e514cc82
Jasurbek NURBOYEV <jasurbeknurboyev@gmail.com>
2024-12-23 19:56:11 +0500

    Fix formatting

--- greed_island/utils/notifications.py
+++ greed_island/utils/notifications.py
@@ -138,7 +138,7 @@ class QuestionNotifier(object):
                         self.strings.resize(self.question.text, max_size=1024, ellipsis_at_end=True)),
                     link_to_message=self.urify.get_message_link(self.question.chat_id, self.question.message_id),
                     thread_link=self.urify.get_message_thread_link(self.question.chat_id, self.question.message_id),
-                    user_name=self.question.author.name,
+                    user_name=self.question.author.full_name,
                 )
                 message: Message = self.bot.send_message(
                     chat_id=group.group_chat_id,
